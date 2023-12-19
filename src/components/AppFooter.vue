36c1d97953bfb119a89bbfe4ed9ee065cef17039
AndreaGul <andreagulino99@gmail.com>
2023-12-19 15:04:42 +0100

    feat:struttura componenti

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,20 @@
 <script>
+import AppFooterInfo from './AppFooterInfo.vue';
+import AppFooterCopyright from './AppFooterCopyright.vue';
+
 export default {
   name: 'AppFooter',
 
-  
+  components: {
+    AppFooterInfo,
+    AppFooterCopyright,
+  },
 };
 </script>
 
 <template>
- 
+  <AppFooterInfo />
+  <AppFooterCopyright />
 </template>
 
 <style scoped lang="scss"></style>
