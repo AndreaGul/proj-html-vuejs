91ec2cd7fdea4353895063c52bb0640b84b0768e
AndreaGul <andreagulino99@gmail.com>
2023-12-20 16:30:10 +0100

    feat:struttura sponsor

--- src/components/components-main/AppMainSponsor.vue
+++ src/components/components-main/AppMainSponsor.vue
@@ -4,6 +4,20 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="row text-center">
+      <!-- come per le card sopra anche qui mi posso creare un oggetto con alliterno i vari dati dei video da inserire con un ciclo -->
+      <div class="col-3">
+        <div>a</div>
+        <p>a</p>
+        <h5>a</h5>
+      </div>
+      <div class="col-3">a</div>
+      <div class="col-3">a</div>
+      <div class="col-3">a</div>
+    </div>
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
