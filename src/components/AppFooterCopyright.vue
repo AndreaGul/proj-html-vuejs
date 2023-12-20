f987c45fa46a6604ffb8e9d4f96f58f87135161e
AndreaGul <andreagulino99@gmail.com>
2023-12-20 17:24:41 +0100

    feat:copyright footer

--- src/components/AppFooterCopyright.vue
+++ src/components/AppFooterCopyright.vue
@@ -4,6 +4,19 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="copyright-container">
+    <div class="container copyright text-center">a</div>
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use 'src/assets/scss/partial/_variables.scss' as *;
+
+.copyright-container {
+  background-color: $primary-bg-black;
+  .copyright {
+    color: white;
+  }
+}
+</style>
