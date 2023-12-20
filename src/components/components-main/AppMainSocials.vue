ccb3b87a3db07456dc8aab6e3b60eb53178813b3
AndreaGul <andreagulino99@gmail.com>
2023-12-20 06:01:56 +0100

    feat:struttura base dei sociasl in main course caracteristics

--- src/components/components-main/AppMainSocials.vue
+++ src/components/components-main/AppMainSocials.vue
@@ -4,6 +4,54 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="row">
+    <div class="col-6 big-card-container">
+      <div class="big-card">a</div>
+    </div>
+    <div class="col-6">
+      <h5>a</h5>
+      <h2>a</h2>
+      <div class="d-flex align-items-center">
+        <div>a</div>
+        <div>
+          <p>a</p>
+          <div><button>a</button><button>a</button></div>
+        </div>
+      </div>
+      <p>a</p>
+      <button>a</button>
+    </div>
+  </div>
+  <div class="container">
+    <div class="d-flex justify-content-between">
+      <h5>a</h5>
+      <h5>a</h5>
+    </div>
+    <div class="row text-center">
+      <!-- come per le card sopra anche qui mi posso creare un oggetto con alliterno i vari dati dei video da inserire con un ciclo -->
+      <div class="col-4">
+        <div>a</div>
+        <h5>a</h5>
+        <p>a</p>
+      </div>
+      <div class="col-4">a</div>
+      <div class="col-4">a</div>
+    </div>
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.big-card-container {
+  position: relative;
+  .big-card {
+    width: 100%;
+    position: absolute;
+    top: -60px;
+
+    background-color: red;
+
+    height: 100px;
+  }
+}
+</style>
