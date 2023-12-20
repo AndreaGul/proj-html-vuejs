9e9baaaa76a383d7d185400b40de3d44d0986f68
AndreaGul <andreagulino99@gmail.com>
2023-12-20 16:22:25 +0100

    feat: stuttura articles

--- src/components/components-main/AppMainArticles.vue
+++ src/components/components-main/AppMainArticles.vue
@@ -4,6 +4,23 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="d-flex justify-content-between">
+      <h5>a</h5>
+      <h5>a</h5>
+    </div>
+    <div class="row text-center">
+      <!-- come per le card sopra anche qui mi posso creare un oggetto con alliterno i vari dati dei video da inserire con un ciclo -->
+      <div class="col-4">
+        <div>a</div>
+        <h5>a</h5>
+        <p>a</p>
+      </div>
+      <div class="col-4">a</div>
+      <div class="col-4">a</div>
+    </div>
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
