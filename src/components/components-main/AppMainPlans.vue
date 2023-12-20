fa9cd75c6ac935b943e99327cb7bfe3e86c8eba4
AndreaGul <andreagulino99@gmail.com>
2023-12-20 16:01:53 +0100

    feat:struttura base plans

--- src/components/components-main/AppMainPlans.vue
+++ src/components/components-main/AppMainPlans.vue
@@ -4,6 +4,42 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="plans-container d-flex justify-content-center">
+    <div class="row container">
+      <!-- creati un oggetti e inseirisci i dati dinamicamente -->
+      <div class="col-4 text-center plans">
+        <div>a</div>
+        <h4>a</h4>
+        <h4>a</h4>
+        <p>a</p>
+        <h5>a</h5>
+      </div>
+      <div class="col-4 text-center plans">
+        <div>a</div>
+        <h4>a</h4>
+        <h4>a</h4>
+        <p>a</p>
+        <h5>a</h5>
+      </div>
+      <div class="col-4 text-center plans">
+        <div>a</div>
+        <h4>a</h4>
+        <h4>a</h4>
+        <p>a</p>
+        <h5>a</h5>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use 'src/assets/scss/partial/_variables.scss' as *;
+
+.plans-container {
+  background-color: $primary-plans-black;
+  .plans {
+    color: white;
+  }
+}
+</style>
