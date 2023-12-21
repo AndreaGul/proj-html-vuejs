1a909e487062ffa2922e73c4da95a0b23a2b1069
AndreaGul <andreagulino99@gmail.com>
2023-12-21 12:57:58 +0100

    feat:suggestions part

--- src/components/components-main/AppMainSuggestionsBanner.vue
+++ src/components/components-main/AppMainSuggestionsBanner.vue
@@ -8,10 +8,12 @@ export default {
   <div class="suggestions-banner-container">
     <div class="gradient-banner">
       <div class="container text-center">
-        <button>a</button>
-        <h2>a</h2>
-        <h5>a</h5>
-        <p class="m-0">a</p>
+        <button class="play-button">
+          <font-awesome-icon icon="fa-solid fa-play" />
+        </button>
+        <h2>Crossfit workout of the day</h2>
+        <h5>November 15thP019</h5>
+        <p class="m-0">View all videos</p>
       </div>
     </div>
   </div>
@@ -22,12 +24,29 @@ export default {
 .suggestions-banner-container {
   background-image: url('src/assets/img/banner14-2x.jpg');
   background-size: cover;
+  background-position: center;
+  color: $primary-text-white;
+
   .gradient-banner {
     background: linear-gradient(
       135deg,
       $primary-gradient-rgba-blu,
       $primary-gradient-rgba-red
     );
+    padding: 150px 0 190px 0;
+    h2 {
+      font-size: 60px;
+      margin: 32px 0 40px 0;
+    }
+    h5 {
+      color: $primary-bg-yellow;
+      font-size: 30px;
+      margin-bottom: 26px;
+    }
+
+    p {
+      font-weight: 500;
+    }
   }
 }
 </style>
