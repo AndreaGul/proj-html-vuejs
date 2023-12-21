e1ebc8912c38e0aa29c84f58c937fd54297ab914
AndreaGul <andreagulino99@gmail.com>
2023-12-21 11:16:59 +0100

    feat:parte articles

--- src/components/components-main/AppMainArticles.vue
+++ src/components/components-main/AppMainArticles.vue
@@ -1,26 +1,95 @@
 <script>
 export default {
   name: 'AppMainArticles',
+
+  data() {
+    return {
+      videos: [
+        {
+          img: '/img/blog1-2x.jpg',
+
+          title: 'The best protein shake',
+          note: 'By admin I November 26th, 201g I Gym',
+        },
+        {
+          img: '/img/blog2-2x.jpg',
+
+          title: 'Ultimate cardio workout',
+          note: 'By admin I November 26th, 20T I Gym',
+        },
+        {
+          img: '/img/blog3-2x.jpg',
+
+          title: 'New juices available now',
+          note: 'By admin I November 26th, 201g I Gym',
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
-  <div class="container">
-    <div class="d-flex justify-content-between">
-      <h5>a</h5>
-      <h5>a</h5>
+  <div class="container general-aticles-container">
+    <div class="upper-video-text d-flex justify-content-between">
+      <h3 class="m-0">Latest articles</h3>
+      <a href="#"
+        ><h6 class="m-0">
+          Read all articles
+          <font-awesome-icon icon="fa-solid fa-angle-right" /></h6
+      ></a>
     </div>
-    <div class="row text-center">
-      <!-- come per le card sopra anche qui mi posso creare un oggetto con alliterno i vari dati dei video da inserire con un ciclo -->
-      <div class="col-4">
-        <div>a</div>
-        <h5>a</h5>
-        <p>a</p>
+    <div class="articles-container d-flex">
+      <div v-for="video in videos" class="article">
+        <div class="article-img-container">
+          <img :src="video.img" alt="img not found" />
+        </div>
+        <h4 class="m-0">{{ video.title }}</h4>
+        <p>{{ video.note }}</p>
       </div>
-      <div class="col-4">a</div>
-      <div class="col-4">a</div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use 'src/assets/scss/partial/_variables.scss' as *;
+
+.general-aticles-container {
+  padding: 130px 0;
+  .upper-video-text {
+    margin-bottom: 46px;
+    a {
+      text-decoration: none;
+      color: #000000;
+
+      h6 {
+        font-size: 18px;
+        font-weight: 600;
+      }
+    }
+  }
+  .articles-container {
+    gap: 52px;
+
+    .article {
+      width: calc((100% - 104px) / 3);
+
+      .article-img-container {
+        width: 100%;
+        cursor: pointer;
+        margin-bottom: 36px;
+
+        img {
+          width: 100%;
+          display: block;
+        }
+      }
+      p {
+        margin-top: 14px;
+
+        color: $primary-text-black-purple;
+      }
+    }
+  }
+}
+</style>
