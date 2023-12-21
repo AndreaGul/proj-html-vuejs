5db1c9d990bf3ce574842a6b7e3f2ad6f57c266e
AndreaGul <andreagulino99@gmail.com>
2023-12-21 04:08:06 +0100

    feat:card presentation

--- src/components/components-main/AppMainPresentation.vue
+++ src/components/components-main/AppMainPresentation.vue
@@ -1,17 +1,46 @@
 <script>
 export default {
   name: 'AppMainPresentation',
+
+  data() {
+    return {
+      services: [
+        {
+          img: '/img/service1.jpg',
+          title: 'Crossfit Workout',
+          note: 'Push your limits',
+        },
+        {
+          img: '/img/service2.jpg',
+          title: 'New gym apparel',
+          note: 'Look good, feel good',
+        },
+        {
+          img: '/img/service3.jpg',
+          title: 'team training',
+          note: 'Find a partener',
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
   <div class="general-container">
     <div class="container presentation-container">
-      <div class="row card-container">
+      <div class="d-flex card-container">
         <!-- questi elementi posono essere inseriti tramite un ciclo for se io inserisco i dati delle card in un oggetto lo posso struttare per riempirle -->
-        <div class="col-4">a</div>
-        <div class="col-4">a</div>
-        <div class="col-4">a</div>
+        <div v-for="service in services" class="p-0 angle-card">
+          <div class="img-container">
+            <img :src="service.img" :alt="service.title" />
+            <div class="text-container">
+              <hr />
+              <h5>{{ service.title }}</h5>
+              <p>{{ service.note }}</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="text-center">
         <div>
@@ -34,21 +63,33 @@ export default {
 <style scoped lang="scss">
 @use 'src/assets/scss/partial/_variables.scss' as *;
 .general-container {
-  position: relative;
-  height: 200px;
   background: linear-gradient(
     135deg,
     $primary-gradient-blue,
     $primary-gradient-red
   );
   .presentation-container {
-    position: absolute;
+    position: relative;
     top: -82px;
-    left: 50%;
-    transform: translate(-50%, 0);
+
     .card-container {
-      background-color: red;
-      height: 100px;
+      gap: 55px;
+      .angle-card {
+        width: calc((100% - 55px) / 3);
+        .img-container {
+          width: 100%;
+          position: relative;
+          img {
+            width: 100%;
+          }
+
+          .text-container {
+            position: absolute;
+            left: 40px;
+            bottom: 40px;
+          }
+        }
+      }
     }
   }
 }
